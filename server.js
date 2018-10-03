40c53e6792fbc721d28422c256b87de2a7f70698
Hung Nguyen <nqhung291@gmail.com>
2018-10-03 18:08:06 +0700

    Initial commit

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ const db = mysql.createConnection({
     password: 'Quanghung291'
 });
 db.connect((err)=>{
-    if(err) throw err;
-    console.log('MySQL Databases connected!');
+    if(err){
+        console.log(err);
+    } else {
+        console.log('MySQL Databases connected!');
+    }
 })
 
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, (err)=>{
     if(err) throw err;
     console.log(`Server started on port ${port}`);
