bb91527240817d066ebcc33d9df2df03c27b67b7
Hung Nguyen <nqhung291@gmail.com>
2018-10-03 15:10:35 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mysql = require('mysql')
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Quanghung291'
+});
+db.connect((err)=>{
+    if(err) throw err;
+    console.log('MySQL Databases connected!');
+})
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, (err)=>{
+    if(err) throw err;
+    console.log(`Server started on port ${port}`);
+})
